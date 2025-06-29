8c8a393e6eb9d29d888149a0b11748c2103211e3
Sejal Gandhi <smg.7246@gmail.com>
2025-06-29 18:44:36 +0530

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+resource "kubernetes_namespace" "openobserve" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "helm_release" "openobserve" {
+  name       = "openobserve"
+  namespace  = kubernetes_namespace.openobserve.metadata[0].name
+
+  # IMPORTANT: use local chart path
+  chart      = "../helm/openobserve"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "namespace" {
+  description = "Namespace for OpenObserve"
+  type        = string
+  default     = "openobserve"
+}
